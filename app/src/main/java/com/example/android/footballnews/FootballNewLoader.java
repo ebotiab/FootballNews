47795a264fb0b13d28c21a1d10b4f22a73b57b8f
Enrique <100406632@alumnos.uc3m.es>
2020-09-02 08:21:15 +0200

    feat: create FootballNewActivity.java

---
+++ app/src/main/java/com/example/android/footballnews/FootballNewLoader.java
@@ -0,0 +1,49 @@
+package com.example.android.footballnews;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import java.util.List;
+
+/**
+ * Loads a list of earthquakes by using an AsyncTask to perform the
+ * network request to the given URL.
+ */
+public class FootballNewLoader extends AsyncTaskLoader<List<FootballNew>> {
+
+    /** Tag for log messages */
+    private static final String LOG_TAG = FootballNewLoader.class.getName();
+
+    /** Query URL */
+    private String mUrl;
+
+    /**
+     * Constructs a new {@link FootballNewLoader}.
+     *
+     * @param context of the activity
+     * @param url to load data from
+     */
+    public FootballNewLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    /**
+     * This is on a background thread.
+     */
+    @Override
+    public List<FootballNew> loadInBackground() {
+        if (mUrl == null) {
+            return null;
+        }
+
+        // Perform the network request, parse the response, and extract a list of earthquakes.
+        List<FootballNew> earthquakes = QueryUtils.fetchFootballNewsData(mUrl);
+        return earthquakes;
+    }
+}
